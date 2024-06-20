083eaf80ab380e3ec5d3bffa6369e6a05cb1e4bd
Eli4118 <99358677+Eli4118@users.noreply.github.com>
2024-06-20 16:28:01 -0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -9,3 +9,5 @@ export const  DB_HOST =  process.env.DB_HOST ||'localhost'
 export const  DB_PORT =  process.env.DB_PORT||3306
 export const  DB_NAME =  process.env.DB_NAME||'seprice'
 export const  FRONT_URL = process.env.FRONT_URL ||'http://localhost:5173'
+
+console.log(PORT, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, BD_NAME, FRONT_URL);
